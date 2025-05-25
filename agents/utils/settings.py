9969bb0ccacb4cfcedb6e29f45439b2b87666135
Ankit Kumar <ankitprasad@hotmail.com>
2025-05-25 19:48:38 +0530

    Adding rag_agent, vector database

--- agents/utils/settings.py
+++ agents/utils/settings.py
@@ -4,8 +4,10 @@ class Settings(BaseSettings):
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore", env_file_encoding="utf-8")
 
-    GROQ_TEXT_MODEL : str = "llama-3.1-8b-instant"
+    GROQ_TEXT_MODEL : str = "meta-llama/llama-4-scout-17b-16e-instruct"
     MAX_TOKENS : int = 1000
     GROQ_API_KEY : str = ""
+    HUGGINGFACE_EMBEDDING_MODEL : str = "sentence-transformers/all-mpnet-base-v2"
+    HUGGINGFACE_EMBEDDING_ARGS : dict = {'device' : 'cpu'}
 
 settings = Settings()
